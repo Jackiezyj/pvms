09dc01595a07c2aed25df22a974c42e4354d351d
Yumei Huang <yumei_huang@wistron.com>
2023-01-03 16:03:52 +0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,17 @@ import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { LoginLabelComponent } from './login-label/login-label.component';
 
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'loginLabel', component: LoginLabelComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    LoginLabelComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
